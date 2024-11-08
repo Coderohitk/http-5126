78acd045c95094e28465262611e02ca120f98b6b
Rohit Kumar <Rohit.kumar1814.rk@gmail.com>
2024-11-08 16:56:15 -0500

    wnwnvi

---
+++ Week 8/HTTP5126-L8-CRUD-KumarRohit.sql
@@ -0,0 +1,48 @@
+--  LAB 8 CRUD
+--  Put your answers on the lines after each letter. E.g. your query for question 1A should go on line 5; your query for question 1B should go on line 7...
+--  1 
+-- A 
+CREATE TABLE customer (email VARCHAR(255),name VARCHAR(255) NOT NULL,PRIMARY KEY (email));
+-- B 
+CREATE TABLE supplier (supplier_id INT AUTO_INCREMENT,name VARCHAR(255) NOT NULL,location VARCHAR(255) NOT NULL,PRIMARY KEY (supplier_id));
+-- C 
+CREATE TABLE `order` (order_id INT AUTO_INCREMENT,email VARCHAR(255) NOT NULL,PRIMARY KEY (order_id),FOREIGN KEY (email) REFERENCES customer(email));
+-- D
+CREATE TABLE product(product_id INT AUTO_INCREMENT,name VARCHAR(255) NOT NULL,price INT NOT NULL,brand VARCHAR(255) NOT NULL,supplier_id INT NOT NULL,PRIMARY KEY (product_id),FOREIGN KEY (supplier_id) REFERENCES supplier(supplier_id));
+-- E 
+CREATE TABLE order_product (order_id INT,product_id INT,PRIMARY KEY (order_id, product_id));
+
+--  2 
+-- A 
+INSERT INTO customer(email,name) VALUES('john@gmail.com','John'),('jane@gmail.com','Jane'),('alice@gmail.com','Alice');
+-- B 
+INSERT INTO supplier(name,location) VALUES('XYZ Electronics','Toronto'),('ABC Gadgets','Montreal'),('XYZ adevnture','Vancouver');
+-- C 
+INSERT INTO `order`(email) VALUES('john@gmail.com'),('jane@gmail.com'),('john@gmail.com'),('alice@gmail.com');
+-- D
+INSERT INTO product(name,price,brand,supplier_id) VALUES('Laptop',800,'Dell',1),('Smartphone',600,'Apple',2),('Smartphone',600,'Samsung',2),('Camera',300,'Canon',1),('Chair',100,'Hermaan Miller',3);
+-- E 
+INSERT INTO order_product(order_id,product_id) VALUES(1,1),(2,2),(2,3),(3,4),(4,5);
+
+--  3 
+-- A 
+ALTER TABLE product ADD CHECK (price>=0);
+-- B 
+UPDATE order_product SET product_id = 4 WHERE order_id = 2 AND product_id = 2;
+
+--  4 
+-- A 
+DELETE FROM order_product WHERE order_id IN (1, 3);
+-- B 
+DELETE FROM `order` WHERE email = 'john@gmail.com';
+
+--Bonus
+-- A 
+ALTER TABLE product DROP FOREIGN KEY product_ibfk_1;
+-- B
+ALTER TABLE product DROP COLUMN supplier_id;
+-- C 
+DROP TABLE supplier;
+
+
+
